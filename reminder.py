21736943551201379849cb10407851777082bf00
coder-ray-8055 <akhtarmdrayyan@gmail.com>
2026-03-08 00:45:35 +0530

    Update reminder.py
    added multi reminder system

--- reminder.py
+++ reminder.py
@@ -1,34 +1,51 @@
 import pyttsx3
 import time
 from plyer import notification
+import threading
 
 engine = pyttsx3.init()
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-    time.sleep(0.2)
 
-def set_reminder():
-    speak("what should i remind you about?")
-    inp = input("what should i remind you about : ")
 
-    speak("After how many second you want to reminded?")
-    inp2 = int(input("After how many seconds you want to reminded : "))
-
-    speak(f"{inp} set for {inp2} seconds")
-    print(f"{inp} set for {inp2} seconds...")
+def reminder_task(inp, inp2):
 
     time.sleep(inp2)
-    speak(f"reminder! {inp}")
-    print(f"reminder! {inp}")
+
+    speak(f"Reminder! {inp}")
+    print(f"Reminder! {inp}")
 
     notification.notify(
-        title = "Reminder",
-        message = f"Time's up! {inp}...",
-        timeout = 5
+        title="Reminder",
+        message=f"Time's up! {inp}",
+        timeout=5
     )
+
+
+def set_reminder():
+
+    while True:
+
+        speak("what should i remind you about?")
+        inp = input("what should i remind you about : ")
+
+        speak("After how many seconds?")
+        inp2 = int(input("After how many seconds : "))
+
+        print(f"{inp} set for {inp2} seconds...")
+
+        t = threading.Thread(target=reminder_task, args=(inp, inp2))
+        t.start()
+
+        again = input("Want to add another reminder(yes/no): ").lower()
+
+        if again != "yes":
+            print("Exitting App...")
+            break
+
 
 if __name__ == "__main__":
     speak("Welcome to your reminder app!")
     set_reminder()
-
